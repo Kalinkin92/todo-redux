08be5c513f3f5fe60f021083be3e4369299ab2ea
Igor <kalinkin92@gmail.com>
2019-08-13 11:23:25 +0300

    state now is storing at the localstorage

--- src/store.js
+++ src/store.js
@@ -1,9 +1,19 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
+import { loadState, saveState } from './localstorage'
 
+
+const persistedState = loadState();
 const store = createStore(
     reducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+    saveState({
+        todoData: store.getState().todoData
+    });
+});
 
 export default store;
